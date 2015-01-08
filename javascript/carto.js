bc1b2f98ebf04a0be53ccc0a5328ddbcaa767ff0
vizurbi <vizurbi@users.noreply.github.com>
2015-01-08 14:10:23 +0100

    Changement du fond de carte

--- javascript/carto.js
+++ javascript/carto.js
@@ -264,8 +264,9 @@ function createMap() {
 	network = new L.LayerGroup();		
 
 	var cmAttr = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-	cmUrl = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
-
+	//cmUrl = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
+	//cmUrl = 'http://{s}.tile.osm.org/hot/{z}/{x}/{y}.png';
+	cmUrl = 'http://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png';
 
 	// Quelques styleId : représentation du fond de la carte: http://maps.cloudmade.com/editor 				
 	var minimal   = L.tileLayer(cmUrl, {styleId: 22677, attribution: cmAttr});
