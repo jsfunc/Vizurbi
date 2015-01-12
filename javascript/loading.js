47e36b92638369b3d015b8f022f0b2963dacca75
vizurbi <vizurbi@users.noreply.github.com>
2015-01-12 11:19:02 +0100

    Cassie

---
+++ javascript/loading.js
@@ -0,0 +1,483 @@
+//function include(src, attributes)
+//{
+//	try {
+//	    	attributes = attributes || {};
+//	        attributes.type = "text/javascript";
+//	        attributes.src = src;
+//	 
+//	        var script = document.createElement("script");
+//	        for(aName in attributes)
+//	            script[aName] = attributes[aName];
+//	 
+//	        document.getElementsByTagName("head")[0].appendChild(script);
+//	        return true;
+//	    } catch(e) { return false; }
+//}
+//
+//include('toolbox.js');
+//include('map.js');
+//include('parameter.js');
+//include('on_off.js');
+//include('math.js');
+
+debug_mode = true;
+
+/*load data */
+function getFile(url, onload){
+	// attention s�curit� : s'assurer que le fichier lu est dans un répertoire donné qui ne contient aucun fichier sensible
+	if (debug_mode) console.log("downloading "+url);
+
+	var xhr_object = null; 
+
+	if(window.XMLHttpRequest) // Firefox 
+		xhr_object = new XMLHttpRequest(); 
+	else if(window.ActiveXObject) // Internet Explorer 
+		xhr_object = new ActiveXObject("Microsoft.XMLHTTP"); 
+	else { // XMLHttpRequest non support� par le navigateur 
+		alert("Votre navigateur ne supporte pas les objets XMLHTTPRequest..."); 
+		return; 
+	} 
+
+	xhr_object.onreadystatechange=function(){
+		if(xhr_object.readyState == 4){
+			if (debug_mode) console.log(url+" complete!");
+			onload(xhr_object.responseText);
+		} 		
+	};
+
+	xhr_object.open("GET", url, true); 
+	xhr_object.send(null); 	
+}
+
+
+function unzipBlob(blob, callback) {
+	zip.createReader(new zip.BlobReader(blob), function(zipReader) {
+		zipReader.getEntries(function(entries) {
+			entries[0].getData(new zip.BlobWriter(zip.getMimeType(entries[0].filename)), function(data) {
+				zipReader.close();
+				callback(data);
+			});
+		});
+	}, function (message) {// error handler
+		if (debug_mode) console.error(message);
+	});
+}
+
+function getZipFile(url, onload){
+	zip.workerScriptsPath = "plugins/zip/";
+//
+//	function unzipBlob(blob, callback) {
+//		zip.createReader(new zip.BlobReader(blob), function(zipReader) {
+//			zipReader.getEntries(function(entries) {
+//				entries[0].getData(new zip.BlobWriter(zip.getMimeType(entries[0].filename)), function(data) {
+//					zipReader.close();
+//					callback(data);
+//				});
+//			});
+//		}, function (message) {// error handler
+//			if (debug_mode) console.error(message);
+//		});
+//	}
+
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', url , true);
+	xhr.responseType = 'blob';
+
+	xhr.onload = function(e) {
+		if (this.status == 200) {
+			// Note: .response instead of .responseText
+			var blob = new Blob([this.response], {type: 'zip'});
+			unzipBlob(blob, handleBlob);
+		}
+	};
+
+	function handleBlob(blob){
+		var reader = new FileReader();
+		reader.onload = function(e) {
+			onload(e.target.result);
+		};
+		reader.readAsText(blob);
+	}
+
+	xhr.send();
+}
+
+function getZipFileWS(url, onload, key){
+	zip.workerScriptsPath = "plugins/zip/";
+//
+//	function unzipBlob(blob, callback) {
+//		zip.createReader(new zip.BlobReader(blob), function(zipReader) {
+//			zipReader.getEntries(function(entries) {
+//				entries[0].getData(new zip.BlobWriter(zip.getMimeType(entries[0].filename)), function(data) {
+//					zipReader.close();
+//					callback(data);
+//				});
+//			});
+//		}, function (message) {// error handler
+//			if (debug_mode) console.error(message);
+//		});
+//	}
+
+	var result=localStorage.getItem(key);	
+	//if (debug_mode) console.log('Result : '+result);
+	if(result!=null) {
+		if (debug_mode) console.log('Getting '+key+' file');
+		onload(result);
+
+	} else {
+
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', url , true);
+		xhr.responseType = 'blob';
+
+		xhr.onload = function(e) {
+			if (this.status == 200) {
+				// Note: .response instead of .responseText
+				var blob = new Blob([this.response], {type: 'zip'});
+				unzipBlob(blob, handleBlob);
+			}
+		};
+		xhr.send();
+	}
+
+	function handleBlob(blob){
+		var reader = new FileReader();
+		reader.onload = function(e) {
+			try{
+				localStorage.setItem(key,e.target.result);
+			}
+			catch (err) {
+				if (debug_mode) console.log(err.message);
+			}
+
+			onload(e.target.result);
+		};
+		reader.readAsText(blob);
+	}
+
+
+}
+
+function load(){
+	if (debug_mode) console.log("loading data and starting app!");
+	createMap(); 
+
+	getZipFile("data/Toulouse/stops.zip", onStopsLoaded); 
+	getZipFile("data/Toulouse/routes.zip", onRoutesLoaded); 
+	getZipFile("data/Toulouse/shapes.zip", onShapesLoaded); 
+	getZipFile("data/Toulouse/calendar.zip", onCalendarLoaded);
+	getZipFile("data/Toulouse/calendar_dates.zip", onCalendar_datesLoaded);
+	getZipFile("data/Toulouse/DWalk.zip", onDWalkLoaded); 
+	getZipFile("data/Toulouse/trips.zip", onTripsLoaded);
+	getZipFile("data/Toulouse/stop_times.zip", onStop_timesLoaded); 
+	getZipFile("data/Toulouse/frequencies.zip", onFrequenciesLoaded);     
+
+}
+
+// loading with web storage
+function loadWS(){
+	if (debug_mode) console.log("loading data and starting app!");
+	createMap(); 
+	getFile("data/Toulouse/download_date.txt", onDownload_dateLoaded);	  	
+}
+
+function getZipFiles(){
+
+	getZipFileWS("data/Toulouse/stops.zip", onStopsLoaded,"stops"); 
+	getZipFileWS("data/Toulouse/routes.zip", onRoutesLoaded,"routes"); 
+	getZipFileWS("data/Toulouse/shapes.zip", onShapesLoaded,"shapes"); 
+	getZipFileWS("data/Toulouse/calendar.zip", onCalendarLoaded,"calendar");
+	getZipFileWS("data/Toulouse/calendar_dates.zip", onCalendar_datesLoaded,"calendar_dates");
+	getZipFileWS("data/Toulouse/DWalk.zip", onDWalkLoaded,"DWalk"); 
+	getZipFileWS("data/Toulouse/trips.zip", onTripsLoaded,"trips");
+	getZipFileWS("data/Toulouse/stop_times.zip", onStop_timesLoaded,"stop_times"); 
+	getZipFileWS("data/Toulouse/frequencies.zip", onFrequenciesLoaded,"frequencies");     
+
+}
+
+
+/* Read files and create date structures */
+function onDownload_dateLoaded(file){
+	var server_date=new Date(file);
+	try {
+		var client=localStorage.getItem("download_date");
+		if (client!=null) {
+			var client_date=new Date(client);		
+			if (debug_mode) console.log(client_date+' '+server_date);			
+			if (server_date.getTime()!=client_date.getTime()) {
+				if (debug_mode) console.log("Removing files");
+				localStorage.clear();
+				localStorage.setItem("download_date",server_date);
+			}
+		}
+		else localStorage.setItem("download_date",server_date);
+	}
+	catch (err)
+	{ if (debug_mode) console.log(err.message);
+	  if (debug_mode) console.log("no local storage available or problem getting download_date");
+	  if (debug_mode) console.log("Removing files");
+	  localStorage.clear();
+	  localStorage.setItem("download_date",server_date);
+		
+	}
+	getZipFiles();
+}
+
+function onStopsLoaded(file){
+	stops = new Array;
+	stopIndex = new Array; 
+	stopName2id = new Array;
+	var flines=file.split(RegExp("\n", "g"));
+	// skip first line: colNames
+	for (var i=1; i<flines.length; i++) if (flines[i].length>0){
+		var descr=flines[i].trim().split(RegExp(",","g"));
+		//stop_id,stop_code,stop_name,stop_lat,stop_lon,location_type,parent_station
+		var id = stops.length;
+		var originalId = String(descr[0]);
+		stopIndex[originalId] = id;
+		var newStop = L.latLng(Number(descr[3]), Number(descr[4]));
+		newStop.isActive = true;
+		newStop.originalId = originalId;		
+		newStop.name = String(descr[2]);
+		newStop.code = String(descr[1]);
+		newStop.parentStation = (descr.length>6) ? String(descr[6]) :"";
+		newStop.subRoutes = new Array;
+		newStop.childStations = new Array;
+		newStop.myName = newStop.name + " " + newStop.code;
+		if (newStop.myName in stopName2id){
+			var s=1;
+			for (j in stopName2id) if (j == newStop.myName + (s==1?"":" "+s)) ++s;
+			newStop.myName = newStop.myName + " " + s;// + " " + newStop.originalId;
+		}
+		stops.push(newStop);
+		//stopName2id[newStop.name] = id; // attention : ne gère pas les doublons ! inutilisable tel quel; assigne une station de départ au hasard parmi celles qui ont ce nom; 
+		stopName2id[newStop.myName] = id; // pour éviter les répétitions de station, soit bien gérer les parentStation, soit juste laisser newStop.name
+		// complete with remaining information...
+	}
+
+	// register childStations
+	for(var i=0; i<stops.length; i++){
+		if (stops[i].parentStation != "") stops[stopIndex[stops[i].parentStation]].childStations.push(i);	
+	}
+
+	if (debug_mode) console.log('onStopsLoaded finished!');
+	onDWalkLoaded();
+	onStop_timesLoaded();
+}
+
+function onRoutesLoaded(file){
+	if (debug_mode) var startTime = performance.now();
+	routes = new Array;
+	routeIndex = new Array;
+	var data = Papa.parse(file, {
+		delimiter: ",",
+		header: false, // a n�gocier!
+		dynamicTyping: false,
+		preview: 0,
+		step: undefined,
+		encoding: "",
+		worker: false,
+		comments: false,
+		complete: undefined,
+		error: undefined,
+		download: false,
+		keepEmptyRows: false,
+		chunk: undefined
+	}).data;
+
+	for (var i=1; i<data.length; i++){
+		var line = data[i];
+		var id = routes.length;
+		var originalId = String(line[0]);
+		routeIndex[originalId] = id;
+		var newRoute = new Object;
+		newRoute.originalId = originalId;
+		newRoute.shortName = line[2];
+		newRoute.longName = line[3];
+		newRoute.type = String(line[5]);
+		newRoute.subRoutes = new Array;
+		routes.push(newRoute);
+	}
+
+	if (debug_mode) console.log('onRoutesLoaded finished!');
+	if (debug_mode) console.log("onRoutesLoaded: "+(performance.now()-startTime));
+	onTripsLoaded();
+}
+
+function onShapesLoaded(file){
+	shapes = new Array;
+	shapeIndex = new Array;
+	var flines=file.split(RegExp("\n", "g"));
+	if (debug_mode) console.log('onShapesLoaded : '+flines.length);
+
+	// skip first line: colNames
+	for (var i=1; i<flines.length; i++) if (flines[i].length>0){
+		var descr=flines[i].split(RegExp(",","g"));
+		//shape_id,shape_pt_lat,shape_pt_lon,shape_pt_sequence
+		var originalId = String(descr[0]);
+		var id=0;
+		if (originalId in shapeIndex){
+			id = shapeIndex[originalId];
+		}
+		else{		
+			var newShape = new Object;
+			newShape.originalId = originalId;
+			newShape.stops = new Array;
+			id = shapes.length;
+			shapes.push(newShape);
+			shapeIndex[originalId] = id;
+		}
+		shapes[id].stops[Number(descr[3])] = new L.latLng(Number(descr[1]), Number(descr[2]));
+	}
+	if (debug_mode) console.log('onShapesLoaded finished!');
+	onTripsLoaded();
+}
+
+function onCalendarLoaded(file){
+	services = new Array;
+	serviceIndex = new Array;
+	var flines=file.split(RegExp("\n", "g"));
+	// skip first line: colNames
+	for (var i=1; i<flines.length; i++) if (flines[i].length>0){
+		var descr=flines[i].split(RegExp(",","g"));	
+		//service_id,monday,tuesday,wednesday,thursday,friday,saturday,sunday,start_date,end_date
+		var id = services.length;
+		var originalId = String(descr[0]);
+		serviceIndex[originalId] = id;
+		var newServ = new Object;
+		newServ.originalId = originalId; // not really necessary?
+		newServ.isOnByDay=new Array(7);
+		for (var j=0; j<7; j++) newServ.isOnByDay[j] = (Number(descr[1+j]) == 1);
+		newServ.startDate = String(descr[8]); // to be treated later!!!
+		newServ.endDate = String(descr[9]);
+		newServ.exceptions = new Array;
+		services.push(newServ);
+	}
+	if (debug_mode) console.log('onCalendarLoaded finished!');
+	onTripsLoaded();
+	onCalendar_datesLoaded();
+}
+
+var _calendar_datesLoaded = 0;
+var fileCalendar_dates;
+function onCalendar_datesLoaded(file){
+	if (file != undefined) fileCalendar_dates = file;
+	if (++_calendar_datesLoaded >= 2){
+		var flines=fileCalendar_dates.split(RegExp("\n", "g"));
+		// skip first line: colNames
+		for (var i=1; i<flines.length; i++) if (flines[i].length>0){
+			var descr=flines[i].split(RegExp(",","g"));
+			//service_id,date,exception_type
+			var id = serviceIndex[String(descr[0])];
+			var date = String(descr[1]);
+			services[id].exceptions[date] = Number(descr[2]);
+		}
+		if (debug_mode) console.log('onCalendar_datesLoaded finished!');
+	}
+}
+
+var _DWalkLoaded = 0;
+var fileDWalk;
+function onDWalkLoaded(file){
+	if (file != undefined) fileDWalk = file;
+	if (++_DWalkLoaded >= 2){
+		dWalk = new Array;
+		for (var i=0; i<stops.length; i++) dWalk[i] = new Array; // in case file is not correct: at least initialize!
+		var flines=fileDWalk.split(RegExp("\n", "g"));
+		if (debug_mode) console.log('onDWalkLoaded : '+flines.length);
+
+
+		for (var i=1; i<flines.length; i++) if (flines[i].length>0){
+			var descr=flines[i].split(RegExp(";","g"));
+			//if (debug_mode) console.log(descr[0]+" "+stopIndex[descr[0]]+" "+ descr[0]+" "+stopIndex[descr[1]]);
+			// StopId, stopId, as_the_crow_flies, by_foot, time_by_foot
+			if (stopIndex[descr[0]]!==undefined && stopIndex[descr[1]]!==undefined){
+				if (descr[3]=="n/a") {
+					dWalk[stopIndex[descr[0]]][stopIndex[descr[1]]] = 10/3600;
+					dWalk[stopIndex[descr[1]]][stopIndex[descr[0]]] = 10/3600;
+				}
+				else{
+					dWalk[stopIndex[descr[0]]][stopIndex[descr[1]]] = Number(descr[4])/3600;
+					dWalk[stopIndex[descr[1]]][stopIndex[descr[0]]] = Number(descr[4])/3600;
+				}
+			}
+		}
+		if (debug_mode) console.log('onDWalkLoaded finished!');
+	}
+}
+
+var _tripsLoaded = 0;
+var fileTrips;
+function onTripsLoaded(file){
+	if (debug_mode) var startTime = performance.now();
+	if (file != undefined) fileTrips = file;
+	if (++_tripsLoaded == 4){
+		var flines=fileTrips.split(RegExp("\n", "g"));
+		if (debug_mode) console.log('onTripsLoaded : '+flines.length);
+
+		_trips= new Array;	
+		_tripIndex = new Array;
+		// skip first line: colNames
+		for (var i=1; i<flines.length; i++) if (flines[i].length>0){
+			var descr=flines[i].split(RegExp(",","g"));
+			//trip_id,service_id,route_id,trip_headsign,direction_id,shape_id
+			var tripId = String(descr[0]);		
+			_tripIndex[tripId] = i-1;
+			newTrip = new Object;
+			newTrip.serviceId = serviceIndex[String(descr[1])];
+			newTrip.routeId = routeIndex[String(descr[2])];
+			newTrip.shapeId = shapeIndex[String(descr[5])];
+			newTrip.stops = new Array;
+			newTrip.times = new Array;
+			newTrip.frequency = {freq:NaN, startTime:0, endTime:0}; // by default, may be updated in readFrequencies
+			_trips.push(newTrip);
+		}
+		if (debug_mode) console.log('onTripsLoaded finished!');
+		if (debug_mode) console.log("onTripsLoaded: "+(performance.now()-startTime));
+		onStop_timesLoaded();
+		affRoutes();
+	}
+}
+
+var _Stop_timesLoaded = 0;
+var fileStop_times;
+function onStop_timesLoaded(file){
+	if (file != undefined) fileStop_times = file;
+	if (++_Stop_timesLoaded >= 3){
+		var flines=fileStop_times.split(RegExp("\n", "g"));
+		// skip first line: colNames
+		for (var i=1; i<flines.length; i++) if (flines[i].length>0){
+			var descr=flines[i].split(RegExp(",","g"));
+			//trip_id,stop_id,stop_sequence,arrival_time,departure_time,stop_headsign,pickup_type,drop_off_type,shape_dist_traveled
+			var tripId = _tripIndex[String(descr[0])];
+			var stopSequence = Number(descr[2]); // index of the stop in the trip
+			_trips[tripId].stops[stopSequence] = stopIndex[String(descr[1])]; 
+			_trips[tripId].times[stopSequence] = readTime(String(descr[3])); 
+			// also load the remaining information
+			// warning: should take both arrival_time and departure_time
+		}
+		if (debug_mode) console.log('onStop_timesLoaded finished!');
+		onFrequenciesLoaded();
+	}
+}
+
+var _FrequenciesLoaded = 0;
+var fileFrequencies;
+function onFrequenciesLoaded(file){
+	if (file != undefined) fileFrequencies = file;
+	if (++_FrequenciesLoaded >= 2){	
+		var flines=fileFrequencies.split(RegExp("\n", "g"));
+		// skip first line: colNames
+		for (var i=1; i<flines.length; i++) if (flines[i].length>0){
+			var descr=flines[i].split(RegExp(",","g"));	
+			//trip_id,start_time,end_time,headway_secs
+			var tripId = _tripIndex[String(descr[0])];
+			var startTime = readTime(String(descr[1]));
+			var endTime = readTime(String(descr[2]));
+			if ((_trips[tripId].times[0] >= startTime) && (_trips[tripId].times[0] <= endTime))// should always be true??? not so clear in specifications
+				_trips[tripId].frequency = {freq:Number(descr[3])/3600, startTime: startTime, endTime: endTime}; 
+		}
+		if (debug_mode) console.log('onFrequenciesLoaded finished!');
+		createSubRoutes();
+	}
+}
---
+++ javascript/parameter.js
@@ -0,0 +1,282 @@
+var basicMode = true;
+debug_mode = true;
+activeRouteCheckColor = "#FF9100"; //"#4f8598"; //FFA200";
+
+function createControls(){
+	if (debug_mode) console.log("starting createControls...");
+	initVariables();
+
+	md = new Date();
+	mh = md.getHours()+md.getMinutes()/60;
+
+
+	$("#slider-minute-vertical").slider({
+		orientation: "vertical",
+		range: "min",
+		min: 1,
+		max: 75,
+		value:30,
+		change: function(event, ui ) {
+			$( "#maxMinute" ).val( ui.value+" mn" );
+			changeMaxMinute();
+		},
+		slide: function(event, ui ) {
+			$( "#maxMinute" ).val( ui.value+" mn" );
+		}
+	});
+	$( "#maxMinute" ).val( $( "#slider-minute-vertical" ).slider( "value" )+" mn" );
+
+
+	$( "#slider-hour-vertical" ).slider({
+		orientation: "vertical",
+		range: "min",
+		min: 2,
+		max: 25.5,
+		value:mh,
+		step:1/60,
+		change: changeStartHour,
+		slide: function(event, ui ) {
+			$( "#startHour" ).val(real2hour(Number(ui.value))); 
+		}
+	}); 
+	$( "#startHour" ).val(real2hour(mh));// real2hour(Number($( "#slider-hour-vertical" ).slider( "value" ))));
+
+
+	$( "#slider-animation-velocity" ).slider({
+		range: "min",
+		min: 0,
+		max: 100,
+		value:50,
+		change: changeAnimationVelocity,
+		slide: changeAnimationVelocity
+	}); 
+	sleepDelay = 1000*Math.exp(-5/100*50);
+
+
+
+//	$.datepicker.setDefaults( $.datepicker.regional[ "fr" ] ); // ça ne marche pas !!!
+	$("#datepicker").datepicker({firstDay: 1, dateFormat: "dd/mm/yy", onSelect:changeDate }).datepicker( "setDate", new Date());
+
+	// initialize control variables and set interactions of IHM
+	var tags = new Array;
+	var alreadyIn = new Array;
+	for (var i=0; i<stops.length; i++) if(stops[i].isActive) { // only parentstations
+		var s = i;
+		if (stops[i].parentStation) s = stopIndex[stops[i].parentStation];
+		if (!alreadyIn[s]){
+			tags.push(stops[s].myName);
+			alreadyIn[s] = true;
+		}
+	}
+	/*
+		if (stops[i].isActive) 
+			tags.push(stops[i].name + " " +stops[i].code);
+	// on pourrait mettre plut�t le nom de la parentStation: 
+			//tags.push(stops[stopIndex[stops[i].parentStation]].name + " " +stops[i].code); // marche pas !
+	 */
+
+	$("#startStation").autocomplete({
+		source: tags, //Object.keys(stopName2id),
+		select: function( event, ui ) {
+			changeStartId(stopName2id[ui.item.value]);
+		},
+		/*	response: function( event, ui ) { // TO BE REMOVED
+			//if (ui.content.length<20) 
+			for (var k=0; k<ui.content.length; k++){
+				stops[stopName2id[ui.content[k].value]].circle.setStyle({fillColor: "#CC0099", fillOpacity:0.8});
+			}
+		},*/
+		focus: function( event, ui ){
+			stops[stopName2id[ui.item.value]].circle.openPopup();
+		}
+	});
+
+	if (debug_mode) console.log("Finished createControls!");
+}
+
+
+function initMapControls(){
+	highLightedPath = L.polyline([stops[1]], {clickable: false, color:"#7E1ED0", opacity:1}).addTo(network); //"#7E1ED0"
+	transitionMarkers = [];
+
+	// stop controls:
+	// pb: dès qu'on touche ici, bug dans l'affichage...
+	// on reste bloqué sur: reading uncompressed file stop_times.txt
+	for (var i=0; i<stops.length; i++) if (stops[i].circle && ((stops[i].parentStation=="") && withOnlyParentStations) || (stops[i].isActive && (!withOnlyParentStations))){ // WARNING: condition is always ok?
+		stops[i].circle.on('click', function(e){
+			newId = e.target.id;
+			//$("#startStation").html(stops[startId].name);
+			changeStartId(newId);	
+			$("#startStation").val(stops[startId].name);
+		});
+		stops[i].circle.on('contextmenu', function(e){
+			map.scrollWheelZoom.disable();
+			affStopInfo(e.target.id);
+
+		});
+		stops[i].circle.on('mouseover', function(e){
+			var departureName = stops[startId].name;
+			var path = getPath(e.target.id);
+			var tt = "<h4>Station " + stops[e.target.id].name+ " : " + Math.floor(60*(arrivalTime[e.target.id]-startHour)) + " minutes</h4>";
+			tt = tt + "<table class=\"pathDescription\">\n<tr><th>" + real2hour(startHour) + "</th><th>" + departureName + "</th></tr>\n";
+			tt = tt + pathDescription(path, departureName);
+			tt = tt + "</table>\n";
+			info.update(tt);
+			/*highLightStopSequence(getDetailedStopSequence(e.target.id));*/
+			highLightPath(path);
+		});
+		stops[i].circle.on('mouseout', function(e){
+			highLightedPath.setLatLngs([stops[startId]]);
+			for (i in transitionMarkers){map.removeLayer(transitionMarkers[i]);}
+			transitionMarkers = [];
+			info.update();
+		});
+	}
+	// route controls:
+	isActiveRoute = new Array(routes.length);
+	activeRouteSpans = new Array(routes.length);
+	for(var i=0; i<routes.length; i++){
+		var s = $("<span class=\"routeCheck\" id=\""+i+"\">"+routes[i].shortName+"</>").css({"background-color":activeRouteCheckColor}); 
+		s.click(function(){if (!basicMode)  toggleRouteActivity($(this).attr('id'));});
+		s.on('contextmenu', function(e) {e.preventDefault(); e.stopPropagation();affRouteInfo($(this).attr('id'));});
+		s.on('mouseover', function(e) {
+			//routes[$(this).attr('id')].polyline.fireEvent("mouseover");
+			var routeId = $(this).attr('id');
+			for (var i in routes[routeId].subRoutes){
+				shapes[subRoutes[routes[routeId].subRoutes[i]].shapeId].polyline.fireEvent("mouseover");
+			}
+		});
+		s.on('mouseout', function(e) {
+			//routes[$(this).attr('id')].polyline.fireEvent("mouseout");}
+			var routeId = $(this).attr('id');
+			for (var i in routes[routeId].subRoutes){
+				shapes[subRoutes[routes[routeId].subRoutes[i]].shapeId].polyline.fireEvent("mouseout");
+			}
+		});
+
+		$("#activeRoutes").append(s);
+		activeRouteSpans[i] = s;
+		isActiveRoute[i] = true;
+	}
+	startApp();
+}
+
+function changeStartId(newId){	
+	//stops[startId].circle.setStyle({"stroke":false,"radius":6});
+	startId=newId;
+	startMarker.setLatLng(stops[startId]);
+	//stops[startId].circle.setStyle({"stroke":true,"radius":10});
+	computeShortestPath();
+	drawAccessible();
+}
+
+
+function startApp(){
+	readDate(); // defines global variable: date, weekDay
+	startHour = $('#slider-hour-vertical').slider("value");
+	maxMinute = $("#slider-minute-vertical").slider("value");
+	setMapInteractions(); // defines global variable: info
+
+	//desactivateUnlinkedStops(); //??? A REVOIR !!! bizarre : �a acc�l�re les choses, mais �a d�sactive toutes les parentStations... qui sont les seules qui ont des points affich�s !
+	// en fait �a efface tout, mais les points actifs seront r�activ�s plus loin
+
+	computeShortestPath(); // defines global variables: comesFrom
+	drawAccessible();	
+	map.spin(false);
+}
+
+
+function changeMaxMinute(e){// affects global variable: maxMinute
+	maxMinute = $('#slider-minute-vertical').slider("value");
+	if (debug_mode) console.log("maxMinute : "+maxMinute+"\n");
+	drawAccessible();
+}
+
+function changeStartHour(event, ui ){// affects global variable: startHour
+	startHour = Number(ui.value);
+	$( "#startHour" ).val(real2hour(startHour)); 
+	if (debug_mode) console.log("startHour : "+startHour+"\n");
+	computeShortestPath();
+	drawAccessible();
+}
+
+
+function changeDate(e){// thru readDate, affects global variables: date, weekDay
+	readDate();
+	computeShortestPath();
+	drawAccessible();
+}
+
+function setMapInteractions(){
+	// information span on the map
+	info = L.control();
+	info.onAdd = function (map) {
+		div = L.DomUtil.create('div', 'info'); 
+		this.update();
+		return div;
+	};
+
+	info.update = function(msg) {
+		div.innerHTML = msg || 'S&eacute;lectionnez la station de d&eacute;part<br/>puis survolez la station d\'arrivée';
+	};
+
+	info.addTo(map);
+
+
+	lineInfo = L.control({position: 'bottomright'});
+	lineInfo.onAdd = function (map) {
+		divLine = L.DomUtil.create('div', 'lineInfo'); 
+		this.update();
+		return divLine;
+	};	
+	lineInfo.update = function(msg) {
+
+		divLine.innerHTML = msg || 'Clic droit pour avoir les horaires de la ligne ou le passage au stop';
+	};
+
+	lineInfo.addTo(map);
+
+	var polylineAlreadySeen = new Array;
+	for (var i=0; i<subRoutes.length; i++) if (!polylineAlreadySeen[shapes[subRoutes[i].shapeId].polyline._leaflet_id]){ // to avoid double event when polyline is used for several subRoutes
+		shapes[subRoutes[i].shapeId].polyline.on({
+			contextmenu:function(e){affRouteInfo(subRoutes[e.target.subRouteId].routeId);},
+			click:function(e){if (!basicMode) toggleRouteActivity(subRoutes[e.target.subRouteId].routeId);},
+			mouseover: highlightRoute,
+			mouseout: resetHighlightRoute
+		});
+		polylineAlreadySeen[shapes[subRoutes[i].shapeId].polyline._leaflet_id] = 1;
+	}
+}
+
+
+function pathDescription(p, lastName){
+	//return(p.map(function(x){return( (x.subRouteId>=0 ? "[via l. "+routes[subRoutes[x.subRouteId].routeId].shortName +"]":"->")+ stops[x.stopId].name);}));
+	s = "";
+	lastName = lastName || "";
+	for(var i in p){
+		if (stops[p[i].stopId].name != lastName){
+			s += "<tr><td></td><td class=path_line_number>";
+			if (p[i].subRouteId>=0){//bus or metro
+				if (routes[subRoutes[p[i].subRouteId].routeId].type==3)//bus
+					s += "<img class=path_route_image src=\"images/bus.png\"\">";
+				else // test metro, =1
+					s += "<img class=path_route_image src=\"images/metro.png\"\">";
+				s += routes[subRoutes[p[i].subRouteId].routeId].shortName+"&nbsp;";
+			}
+			else{//walk
+				s += "<img class=path_route_image src=\"images/walk.png\"\">";
+			}
+			s += "</tr><tr><td>" + real2hour(arrivalTime[p[i].stopId]) + "</td>";
+			s += "<td>" + stops[p[i].stopId].name + "</td></tr>\n";
+			lastName = stops[p[i].stopId].name;
+		}
+	}
+	return(s);
+}
+
+
+/* Day movie */
+function changeAnimationVelocity(event, ui){
+	sleepDelay = 1000*Math.exp(-5/100*Number(ui.value));//$('#slider-animation-velocity').slider("value")));
+	if (debug_mode) console.log("sleepDelay : "+sleepDelay+"\n");
+}
